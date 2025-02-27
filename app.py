fa586d35b40f07377dcc7a4067ffb7a6a6d4c841
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2025-02-27 10:36:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ ab_u = unico['CLAVES'].unique()
 ab_s = simultaneo['CLAVES'].unique()
 
 # Definir funciones para crear gráficos
-def crear_pie(data, column):
-    return px.pie(data, names=column)
+def crear_pie(data):
+    data['Tipo'] = data['CLAVES'].apply(lambda x: 'Medicamento' if int(x.split('.')[0]) < 60 else 'Material de Curación')
+    return px.pie(data, names='Tipo', color='Tipo', color_discrete_map={'Medicamento': 'blue', 'Material de Curación': 'red'})
 
 # Configuración de la página
 st.set_page_config(page_title="Dashboard", layout="wide")
@@ -67,7 +68,7 @@ cl = [clave_input] if clave_input != "TODAS LAS CLAVES" else claves_unicas
 datos_filtrados = df[(df['CLAVES'].isin(cl)) & (df['CLAVES'].isin(abastecimiento)) & (df['CLAVES'].isin(ty))]
 
 # Mostrar gráficos 
-st.plotly_chart(crear_pie(datos_filtrados, 'CLAVES'), key="resumen_pie_oferta")
+st.plotly_chart(crear_pie(datos_filtrados), key="resumen_pie_oferta")
 
 # Incluir imagen como pie de página
 st.image("footer.png", use_container_width=True)
