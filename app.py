27e8f7e8e5e0000c71683965954f0ee59fec328d
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2025-03-04 19:20:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -224,60 +224,66 @@ with tab1:
     if periodo_input == "BIANUAL":
         df1 = datos_filtradosbi
         df2 = datos_filbi
+        df1T = "CANTIDAD DEMANDADA BIANUAL"
+        df2T = "IMPORTE BIANUAL"
     elif periodo_input == "2025":
         df1 = datos_filtrados25
         df2 = datos_fil25
+        df1T = "CANTIDAD DEMANDADA 2025"
+        df2T = "IMPORTE 2025"
     else:
         df1 = datos_filtrados26
         df2 = datos_fil26
-    
+        df1T = "CANTIDAD DEMANDADA 2026"
+        df2T = "IMPORTE 2026"    
+        
     # Crear columnas
     col1, col2, col3 = st.columns(3)
     
     # Mostrar gráficos en columnas
     with col1:
         st.header("Tipo de Clave Bianual")
-        st.plotly_chart(crear_pie(datos_filtradosbi), key="resumenbi_pie_oferta")
+        st.plotly_chart(crear_pie(df1), key="resumenbi_pie_oferta")
         st.header("Tipo de Abastecimiento Bianual")
-        st.plotly_chart(crear_hist(datos_filbi), key="resumenbi_hist_oferta")
+        st.plotly_chart(crear_hist(df1), key="resumenbi_hist_oferta")
 
-        st.header("Canitdades bianual")
-        filqbi = visual("TOTAL", datos_filtradosbi)
-        for a, filb in enumerate(filqbi):
-            st.plotly_chart(filb, key=f"filb_{a}")
-        st.header("Importe bianual")
-        filmbi = visualMonto("TOTAL", datos_filbi)
-        for b, filmb in enumerate(filmbi):
-            st.plotly_chart(filmb, key=f"filmb_{b}")
-        st.dataframe(datos_filtradosbi)
+#        st.header("Canitdades bianual")
+ #       filqbi = visual("TOTAL", datos_filtradosbi)
+  #      for a, filb in enumerate(filqbi):
+   #         st.plotly_chart(filb, key=f"filb_{a}")
+    #    st.header("Importe bianual")
+     #   filmbi = visualMonto("TOTAL", datos_filbi)
+      #  for b, filmb in enumerate(filmbi):
+       #     st.plotly_chart(filmb, key=f"filmb_{b}")
+        #st.dataframe(datos_filtradosbi)
     
     with col2:
-        st.header("Tipo de Clave 2025")
-        st.plotly_chart(crear_pie(datos_filtrados25), key="resumen25_pie_oferta")
-        st.header("Tipo de Abastecimiento 2025")
-        st.plotly_chart(crear_hist(datos_fil25), key="resumen25_hist_oferta")
+ #       st.header("Tipo de Clave 2025")
+  #      st.plotly_chart(crear_pie(datos_filtrados25), key="resumen25_pie_oferta")
+   #     st.header("Tipo de Abastecimiento 2025")
+    #    st.plotly_chart(crear_hist(datos_fil25), key="resumen25_hist_oferta")
 
-        st.header("Cantidades 2025")
-        filq25 = visual("TOTAL", datos_filtrados25)
+        st.header("df1T")
+        filq25 = visual("TOTAL", df1)
         for c, filg25 in enumerate(filq25):
             st.plotly_chart(filg25, key=f"filg25_{c}")
-        st.header("Importe 2025")
-        film25 = visualMonto("TOTAL", datos_fil25)
-        for d, filc25 in enumerate(film25):
-            st.plotly_chart(filc25, key=f"filc25_{d}")
+#        st.header("Importe 2025")
+ #       film25 = visualMonto("TOTAL", datos_fil25)
+  #      for d, filc25 in enumerate(film25):
+   #         st.plotly_chart(filc25, key=f"filc25_{d}")
 
     with col3:
-        st.header("Tipo de Clave 2026")
-        st.plotly_chart(crear_pie(datos_filtrados26), key="resumen26_pie_oferta")
-        st.header("Tipo de Abastecimiento 2026")
-        st.plotly_chart(crear_hist(datos_fil26), key="resumen26_hist_oferta")
+#        st.header("Tipo de Clave 2026")
+ #       st.plotly_chart(crear_pie(datos_filtrados26), key="resumen26_pie_oferta")
+  #      st.header("Tipo de Abastecimiento 2026")
+   #     st.plotly_chart(crear_hist(datos_fil26), key="resumen26_hist_oferta")
 
-        st.header("Cantidades 2026")
-        filq26 = visual("TOTAL", datos_filtrados26)
-        for c, filg26 in enumerate(filq26):
-            st.plotly_chart(filg26, key=f"filg26_{c}")
-        st.header("Importe 2026")
-        film26 = visualMonto("TOTAL", datos_fil26)
+    #    st.header("Cantidades 2026")
+     #   filq26 = visual("TOTAL", datos_filtrados26)
+      #  for c, filg26 in enumerate(filq26):
+       #     st.plotly_chart(filg26, key=f"filg26_{c}")
+        st.header("df2T")
+        film26 = visualMonto("TOTAL", df2)
         for d, filc26 in enumerate(film26):
             st.plotly_chart(filc26, key=f"filc26_{d}")
 
