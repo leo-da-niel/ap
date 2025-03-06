6fc9324af4bf250e560db0d270744b79fd2440cd
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2025-03-06 15:00:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -484,7 +484,7 @@ with tab3:
     #for j, fig in enumerate(figures):
      #   st.plotly_chart(fig, key=f"fig_{j}")
     # Incluir información general
-    st.plotly_chart(cloud_bubbles_prov(bi), key="prov-top10")
+    st.plotly_chart(cloud_bubbles_prov(bi), key=f"prov-top10")
     st.dataframe(datos_filtrados)
     
 # Incluir imagen como pie de página
