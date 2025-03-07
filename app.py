ece3f5456d0d171dffd8a11caa3cee2ac2f32fe3
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2025-03-07 12:18:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -535,7 +535,7 @@ with tab3:
         ax.set_ylabel('Proveedor', fontsize=24)
         ax.set_title('Top 10 Proveedores con Más Claves Adjudicadas', fontsize=24)
         for bar in bars:
-            ax.annotate(f'{bar.get_width()}', xy=(bar.get_width(), bar.get_y() + bar.get_height() / 2), xytext=(5, 0), textcoords='offset points', ha='left', va='center')
+            ax.annotate(f'{bar.get_width()}', xy=(bar.get_width(), bar.get_y() + bar.get_height() / 2), xytext=(5, 0), textcoords='offset points', ha='left', va='center', , fontsize=24)
         st.header("TOP 10 PROVEEDORES CON MÁS ADJUDICACIONES")
         st.pyplot(fig)
 
